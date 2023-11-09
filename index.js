5cb2486182bac8abcb6c5e03fc982e65f09fb022
Souravn1200 <souravn1200@gmail.com>
2023-11-09 15:19:03 +0600

    Jwt token sending

--- index.js
+++ index.js
@@ -74,6 +74,7 @@ async function run() {
     app.post('/logout', async (req, res) => {
       const user = req.body
       console.log(user);
+      
       res.clearCookie("token", { maxAge: 0 }).send({ success: true });
     })
 
