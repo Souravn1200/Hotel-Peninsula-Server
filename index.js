4926fac1efdd67bf0bcd795e2426af94aa93f7ec
Souravn1200 <souravn1200@gmail.com>
2023-11-09 15:21:42 +0600

    Jwt token sign

--- index.js
+++ index.js
@@ -61,7 +61,6 @@ async function run() {
     app.post('/jwt', async (req, res) => {
 
       const user = req.body;
-
       console.log(user);
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
       res
